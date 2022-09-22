bd5319729df4f2eb9657206206ca1375a7fadff3
SeHeon Ho <ho78901@naver.com>
2022-09-22 11:59:18 +0900

    경로 수정 test

--- blog/tests.py
+++ blog/tests.py
@@ -6,10 +6,22 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
-        def navbar_test(self, soup):
-            navbar = soup.nav
-            self.assertIn('Blog', navbar.text)
-            self.assertIn('About Me', navbar.text)
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        logo_btn = navbar.find('a', text='Do It Django')
+        self.assertEqual(logo_btn.attrs['href'], '/')
+
+        home_btn = navbar.find('a', text='Home')
+        self.assertEqual(home_btn.attrs['href'], '/')
+
+        blog_btn = navbar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+
+        about_me_btn = navbar.find('a', text='About Me')
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
 
     def test_post_list(self):
         # 1.1 포스트 목록 페이지를 가져온다.
